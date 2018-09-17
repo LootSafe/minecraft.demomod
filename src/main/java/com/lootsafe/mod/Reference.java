85eafe0e9c1fbc989204fbc1a481076bee8d5b0d
SoftDevAndy <sweeneyandrew.softdev@gmail.com>
2018-09-17 16:22:28 -0400

    Latest Jars & Minor Update

--- src/main/java/com/lootsafe/mod/Reference.java
+++ src/main/java/com/lootsafe/mod/Reference.java
@@ -10,7 +10,7 @@ public class Reference {
 
 	public static final String MOD_ID = "lsm";
 	public static final String NAME = "LootSafe Mod";
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "1.0 alpha";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.lootsafe.mod.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.lootsafe.mod.proxy.CommonProxy";
