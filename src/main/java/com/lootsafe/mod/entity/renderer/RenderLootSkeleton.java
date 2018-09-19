0217ef175e4cb14ab81d705e3413b11a480dc854
SoftDevAndy <sweeneyandrew.softdev@gmail.com>
2018-09-19 01:00:16 -0400

    Animations Animations walking/hitting weren't there for the custom renderer, so using the default right now. Enemies are animated agains.

--- src/main/java/com/lootsafe/mod/entity/renderer/RenderLootSkeleton.java
+++ src/main/java/com/lootsafe/mod/entity/renderer/RenderLootSkeleton.java
@@ -4,6 +4,7 @@ import com.lootsafe.mod.Reference;
 import com.lootsafe.mod.entity.EntityLootSkele;
 import com.lootsafe.mod.entity.model.ModelLootSkeleton;
 
+import net.minecraft.client.model.ModelSkeleton;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
@@ -13,7 +14,8 @@ public class RenderLootSkeleton extends RenderLiving<EntityLootSkele> {
 	public static final ResourceLocation TEXTURES = new ResourceLocation(Reference.MOD_ID + ":textures/entity/lootskele.png");
 	
 	public RenderLootSkeleton(RenderManager manager, float skeleSize){
-		super(manager, new ModelLootSkeleton(skeleSize), skeleSize);
+		//super(manager, new ModelLootSkeleton(skeleSize), skeleSize);
+		super(manager, new ModelSkeleton(), skeleSize);
 	}
 	
 	protected ResourceLocation getEntityTexture(EntityLootSkele entity){
--- src/main/java/com/lootsafe/mod/entity/renderer/RenderLootSpider.java
+++ src/main/java/com/lootsafe/mod/entity/renderer/RenderLootSpider.java
@@ -4,6 +4,7 @@ import com.lootsafe.mod.Reference;
 import com.lootsafe.mod.entity.EntityLootSpider;
 import com.lootsafe.mod.entity.model.ModelLootSpider;
 
+import net.minecraft.client.model.ModelSpider;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
@@ -13,7 +14,8 @@ public class RenderLootSpider extends RenderLiving<EntityLootSpider> {
 	public static final ResourceLocation TEXTURES = new ResourceLocation(Reference.MOD_ID + ":textures/entity/lootspider.png");
 	
 	public RenderLootSpider(RenderManager manager, float spiderSize){
-		super(manager, new ModelLootSpider(spiderSize), spiderSize);
+		//super(manager, new ModelLootSpider(spiderSize), spiderSize);
+		super(manager, new ModelSpider(), spiderSize);
 	}
 	
 	protected ResourceLocation getEntityTexture(EntityLootSpider entity){
